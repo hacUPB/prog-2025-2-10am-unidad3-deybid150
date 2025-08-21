5513e06a9df6520c8a3aa417d2c688f094fd24a5
deybid castillo <deybid.castillo@upb.edu.co>
2025-08-21 10:49:48 -0500

    adicion del IMC en saludo

--- src/saludo.py
+++ src/saludo.py
@@ -1,4 +1,12 @@
 print("¿como te llamas(nombre y apellido)")
 nombre = input()
 print ("bienvenido: ", nombre)
-       
+
+#calcular el IMC de esa persona
+peso = input ("ingresa tu peso en kilogramos: ")
+peso = float(peso)
+altura = input ("ingresa tu estatura en metros: ")
+altura = float(altura)
+IMC = peso/altura**2
+
+print("tu IMC es igual a: ", IMC)
