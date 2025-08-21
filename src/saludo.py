61b2ef6a0ef6d59f9e87d896e1dbd8c647218144
deybid castillo <deybid.castillo@upb.edu.co>
2025-08-21 11:30:59 -0500

    final del ej 2

--- src/saludo.py
+++ src/saludo.py
@@ -9,4 +9,19 @@ altura = input ("ingresa tu estatura en metros: ")
 altura = float(altura)
 IMC = peso/altura**2
 
-print(nombre, "tu IMC es igual a: ", IMC)
+if 18.5 < IMC < 24.9:
+    estatus = "normal"
+else:
+    if 25 < IMC < 29.9:
+        estatus = "sobre peso" 
+    else:
+        if 30 < IMC < 34.9:
+            estatus = "obesidad 1"
+        else: 
+            if 35 < IMC < 39.9:
+                estatus = "obesidad 2"
+            else:
+                if IMC >= 40:
+                    estatus = "obesidad extrema 3"
+
+print(nombre, "tu IMC es igual a: ", IMC, "tu estatus es: ", estatus)
