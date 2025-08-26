15bcbe38d2d45a17f6832f55b25afd3bd0c4e320
deybid castillo <deybid.castillo@upb.edu.co>
2025-08-26 10:40:42 -0500

    uso del elif y reemplazo de if y else por elif

--- src/saludo.py
+++ src/saludo.py
@@ -9,18 +9,20 @@ altura = input ("ingresa tu estatura en metros: ")
 altura = float(altura)
 IMC = peso/altura**2
 
-if 18.5 < IMC < 24.9:
-    estatus = "normal"
-else:
-    if 25 < IMC < 29.9:
-        estatus = "sobre peso" 
-    else:
-        if 30 < IMC < 34.9:
-            estatus = "obesidad 1"
-        else: 
-            if 35 < IMC < 39.9:
-                estatus = "obesidad 2"
-            else:
-                if IMC >= 40:
-                    estatus = "obesidad extrema 3"
-print(nombre, "tu IMC es igual a: ", IMC, "tu estatus es: ", estatus)
+if IMC < 18.5:
+    estatus = "desnutricion tipo guajira"
+elif 18.5 < IMC < 24.9:
+        estatus = "normal"
+elif 25 < IMC < 29.9:
+            estatus = "sobre peso" 
+elif 30 < IMC < 34.9:
+                estatus = "obesidad 1"
+elif 35 < IMC < 39.9:
+                    estatus = "obesidad 2"
+elif IMC >= 40:
+                        estatus = "obesidad extrema 3"
+print(f" {nombre}, tu IMC es igual a: {IMC:0.2f}, tu estatus es: {estatus}")
+
+
+
+
