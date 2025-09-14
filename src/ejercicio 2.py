5b228222026ae8c6bf90ace555932c4c6ad7e241
deybid150 <deybid.castillo@upb.edu.co>
2025-09-14 16:57:29 -0500

    Revert "Feedback"

--- src/ejercicio 2.py
+++
@@ -1,7 +0,0 @@
-#par o impar
-numero = int(input("ingrese el numero entero"))
-residuo = numero % 2
-if residuo == 0:
-    print(numero, "es par")
-else: 
-     print(numero, "es impar")
--- src/tablas_de_multiplicacion.py
+++
@@ -1,6 +0,0 @@
-numero = float(input("ingrese un numero"))
-i = 1
-while i <= 15:
-    resultado = numero * i
-    print(f"{numero}x{i} = {resultado}")
-    i += 1
